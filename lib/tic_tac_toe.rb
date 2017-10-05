0734f0e4af8f9216784453732bc68836b6318c8f
Daniel Rios <danielrafaelrios@gmail.com>
2017-10-05 21:11:27 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,10 +31,7 @@ def move(index, current_player)
   @board[index] = current_player
 end
 
-def position_taken?(index)
-  if @board[index] == "X" || @board[index] == "O")
-    true
-  end
+
 
 def valid_move?(board,index)
 if index.between?(0, 8) && !position_taken?(board,index)
