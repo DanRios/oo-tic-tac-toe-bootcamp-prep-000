c8cab7c36eb8ad194ecab340372822eead604428
Daniel Rios <danielrafaelrios@gmail.com>
2017-10-05 20:30:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,4 +16,11 @@ WIN_COMBINATIONS = [
   [2, 4, 6] #right_diagonal_win
 ]
 
+def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+
   
